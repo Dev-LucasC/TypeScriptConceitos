e2ec1005b0d573ea9f181d9303cd3a42f8a2819f
Lucas <lucaconceicao@hotmail.com>
2023-10-03 12:38:15 -0300

    Conceitos

--- dist/js/index.js
+++ dist/js/index.js
@@ -12,3 +12,41 @@ const user = {
 };
 console.log(user);
 //Variavel Any nao é aceita em TypeScript
+// Union Type
+let id = "10";
+id = 200;
+id = 200;
+const userId = 10;
+// Enum
+// tamanho de roupas (size: Médio, size: Pequeno)
+var Size;
+(function (Size) {
+    Size["P"] = "Pequeno";
+    Size["M"] = "M\u00E9dio";
+    Size["G"] = "Grande";
+})(Size || (Size = {}));
+const camisa = {
+    name: "Camisa Gola V",
+    size: Size.G,
+};
+console.log(camisa);
+// Literal Types
+let teste;
+//teste = "outrovalor"
+teste = "algumvalor";
+teste = null;
+// Teste de uso por ex: Validar login = login (suces) | Null (Fail)
+//funcoes
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 12));
+// Errado: console.log(sum( "12", true));
+function sayHelloTo(name) {
+    return `Hello ${name}`;
+}
+console.log(sayHelloTo("Lucas"));
+function logger(msg) {
+    console.log(msg);
+}
+logger("Teste!");
--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,84 @@ console.log(user);
 
 //Variavel Any nao é aceita em TypeScript
 
+
+// Union Type
+let id: string | number = "10"
+
+id = 200
+
+id = 200
+
+// id = true
+// id = []
+
+// type alias 
+type myIdType = number | string 
+
+const userId: myIdType = 10
+
+// Enum
+// tamanho de roupas (size: Médio, size: Pequeno)
+
+enum Size {
+    P = "Pequeno",
+    M = "Médio",
+    G = "Grande"
+}
+
+const camisa = {
+    name: "Camisa Gola V",
+    size: Size.G,
+};
+
+console.log(camisa);
+
+
+// Literal Types
+
+let teste: "algumvalor" | null
+
+//teste = "outrovalor"
+
+teste = "algumvalor"
+teste = null;
+
+
+// Teste de uso por ex: Validar login = login (suces) | Null (Fail)
+
+//funcoes
+
+function sum(a: number, b: number) {
+    return a + b
+}
+
+console.log(sum(12, 12));
+
+// Errado: console.log(sum( "12", true));
+
+function sayHelloTo(name: string): string {
+    return `Hello ${name}`
+}
+
+console.log(sayHelloTo("Lucas"));
+
+
+function logger(msg: string): void {
+    console.log(msg)
+}
+
+logger("Teste!")
+
+
+// Narrowing - checagem de tipos
+
+function doSomething(info: number | boolean) {
+    if(typeof info === "number"){
+        console.log(`o número é ${info}` );
+        return;
+    }
+    console.log("Nao foi passado um número")
+}
+
+doSomething(5);
+doSomething(true);
