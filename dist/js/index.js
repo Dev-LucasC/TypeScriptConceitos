58eb7fec40b1d081308c0f2f7f136c6b0aaddc12
Lucas <lucaconceicao@hotmail.com>
2023-10-03 09:46:55 -0300

    Teste

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,14 @@
 "use strict";
+let x = 10;
+console.log(x);
+//tuplas 
+let myTuple;
+myTuple = [5, "teste", ["a", "b"]];
+//myTuple = [true, false, true]  --- Errado
+// Object Literals -> {prop: value}
+const user = {
+    name: "Pedro",
+    age: 18,
+};
+console.log(user);
+//Variavel Any nao é aceita em TypeScript
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 let x: number = 10;
 
 console.log(x);
+
+
+
+//tuplas 
+
+let myTuple: [number, string, string[]]
+
+myTuple = [5, "teste", ["a", "b"]]
+
+
+//myTuple = [true, false, true]  --- Errado
+
+
+// Object Literals -> {prop: value}
+
+const user: {name: string, age: number} = {
+    name: "Pedro",
+    age: 18,
+};
+
+console.log(user);
+
+//Variavel Any nao é aceita em TypeScript
+
